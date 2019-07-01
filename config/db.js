91d87b8a145c089def6250608adae10eb8089c76
Julian Figueroa <julianfigueroatorres@gmail.com>
2019-06-30 20:54:14 -0500

    MongoDB was initialized from Server

---
+++ config/db.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const options = {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+};
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.mongoURI, options);
+    console.log('Connected to MongoDB');
+  } catch (err) {
+    console.error('Connected to mLab DB', err);
+  }
+};
+
+module.exports = connectDB;
